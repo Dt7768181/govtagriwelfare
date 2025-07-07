7e21d2cae8f5b2b76b458010e038c8c251565979
dt7768181 <dt7768181@gmail.com>
2025-07-07 08:57:16 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <div className="header-container">
+        <h2>GOV4FARMERS</h2>
+        <button>Login</button>
+      </div>
+      <br></br>
+      <div class="body-container">
+        <h2>Available schemes</h2>
+      </div>
+    </div>
+  );
+}
+export default App;
+
