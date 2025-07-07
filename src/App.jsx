d565e73705f3cfcdf6e47a31ed53a899465da75b
dt7768181 <dt7768181@gmail.com>
2025-07-07 10:20:00 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import LoginPage from './LoginPage'; 
+import './app.css'; 
+
 function App() {
   return (
     <div>
       <div className="header-container">
         <h2>GOV4FARMERS</h2>
-        <button>Login</button>
-      </div>
+        <Link to={'/'}><button>Login</button></Link>
+        
+</div>
       <br></br>
-      <div class="body-container">
+      <div className="body-container">
         <h2>Available schemes</h2>
+        <marquee>Trending</marquee>
+        <div class="scheme-container">
+          <div class="scheme-row">
+            <div className="scheme-card">
+              <h3>Pradhan Mantri Kisan Samman Nidhi </h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+            <div className="scheme-card">
+              <h3>Pradhan Mantri Fasal Bima Yojana (PMFBY)</h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+            <div className="scheme-card">
+              <h3>Soil Health Card Scheme</h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+          </div>
+          <div class="scheme-row">
+            <div className="scheme-card">
+              <h3>Rashtriya Krishi Vikas Yonaja (RKVY)</h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+            <div className="scheme-card">
+              <h3>e-NAM(National Agriculture Market)</h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+            <div className="scheme-card">
+              <h3>PM Jeevan Beej Yojana</h3>
+              <p>Brief description of the scheme goes here.</p>
+              <button>Learn More</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/LoginPage.jsx
@@ -0,0 +1,6 @@
+const LoginPage = () => {
+    return <h2>This is the login page</h2>;
+  };
+  
+  export default LoginPage;
+  
